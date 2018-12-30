76d45f891d36775efd6c1ed232b664e8afb29619
Hyago Hirai <hyahirai@hotmail.com>
2018-12-30 13:59:41 -0200

    First Commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var myBankBalance = 0;
+var output = "";
+
+// Do the 'for' loop
+for (myBankBalance = 0; myBankBalance <= 10; myBankBalance++) {
+    output += "My bank balance is now $" + myBankBalance + "<br>";
+}
