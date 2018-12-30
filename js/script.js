6018358bd6195f6922f8102323656b7d175e7067
Hyago Hirai <hyahirai@hotmail.com>
2018-12-30 14:16:23 -0200

    Add new variable fernando

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 var myBankBalance = 2;
 var output = "";
+<<<<<<< HEAD
+=======
+var hyago = "hyago";
+var fernando = "fernando";
+>>>>>>> Add new variable fernando
 
 // Do the 'for' loop
 for (myBankBalance = 0; myBankBalance <= 10; myBankBalance++) {
