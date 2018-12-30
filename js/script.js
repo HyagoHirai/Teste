4fe98a94519da21a777f2427a2eaebdef8fe8934
Hyago Hirai <hyahirai@hotmail.com>
2018-12-30 19:05:52 -0200

    fix conflict

--- js/script.js
+++ js/script.js
@@ -1,14 +1,6 @@
 var myBankBalance = 2;
-<<<<<<< HEAD
-var output = "";
-var hyago = "hyago";
-var fernando = "fernando";
-var lucas = "lucas";
-var alvaro = "alvaro"
-=======
 var output = "Hyago";
 var teste = "teste"
->>>>>>> teste-merge
 
 // Do the 'for' loop
 for (myBankBalance = 0; myBankBalance <= 10; myBankBalance++) {
